3b3ad66a80c4878a381b252015f077b5b83bc2e3
Asdow <20314541+Asdow@users.noreply.github.com>
2025-01-02 16:49:52 +0200

    ItemFlag has been expanded from int32 -> int64

--- BackEnd/LookupFile.vb
+++ BackEnd/LookupFile.vb
@@ -1,7 +1,7 @@
 Imports System.Data
 
 Public Class LookupFile
-    Private Shared Sub AddLookupData(ByVal t As DataTable, ByVal id As Integer, ByVal name As String)
+    Private Shared Sub AddLookupData(ByVal t As DataTable, ByVal id As Long, ByVal name As String)
         Dim r As DataRow = t.NewRow
         r("id") = id
         r("name") = name
@@ -15,7 +15,7 @@ Public Class LookupFile
         curNode = ""
 
         Dim name As String = Nothing
-        Dim id As Integer = Integer.MinValue
+        Dim id As Long = Long.MinValue
 
         While xr.Read
             If xr.NodeType = Xml.XmlNodeType.Element Then
@@ -24,19 +24,19 @@ Public Class LookupFile
                 curValue = xr.Value
                 Select Case curNode
                     Case "id"
-                        id = Integer.Parse(curValue)
+                        id = Long.Parse(curValue)
                     Case "name"
                         name = curValue
                 End Select
             End If
 
             ' Both variables are set
-            If name <> Nothing And id <> Integer.MinValue Then
+            If name <> Nothing And id <> Long.MinValue Then
                 AddLookupData(lookupTable, id, name)
 
                 'Reset
                 name = Nothing
-                id = Integer.MinValue
+                id = Long.MinValue
             End If
 
         End While
